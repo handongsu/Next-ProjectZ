62a64abb5b555af96383f1d285693e8e28e51aa9
dongsu.han <kku304@naver.com>
2024-05-15 00:16:52 +0900

    postform layout

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,20 @@
+import Post from "../_component/Post";
+import style from "./home.module.css";
+import Tab from "./_component/Tab";
+import PostForm from "./_component/PostForm";
+
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <Tab />
+      <PostForm />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </main>
-  )
+  );
 }
